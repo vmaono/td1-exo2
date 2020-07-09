80c53de3b1209c3c8d0b08970d68818ac8d8db4c
vmaono <68002848+vmaono@users.noreply.github.com>
2020-07-09 16:13:58 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index < 2; index++) {
+    basic.showString("bonjour")
+    basic.pause(500)
+    basic.showNumber(10850)
+    basic.pause(500)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+}
+basic.forever(function () {
+	
+})
